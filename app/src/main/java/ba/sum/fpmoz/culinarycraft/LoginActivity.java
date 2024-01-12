00df2ed9e024aadc6f6392caedf70451f7e4c532
IvanJurišić <ivanjur8@gmil.com>
2024-01-12 21:59:22 +0100

    Login i registracija

---
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/LoginActivity.java
@@ -0,0 +1,24 @@
+package ba.sum.fpmoz.culinarycraft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn=findViewById(R.id.btn_registriraj_se);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
@@ -2,6 +2,7 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
