f0fcc15956ffbcbd492bc2fd26aeac16daf0a161
ljdedic <ljiljana.dedic@fpmoz.sum.ba>
2024-01-16 12:11:09 +0100

    povezivanje na Firebase i izmjene kod login i registra

--- app/src/main/java/ba/sum/fpmoz/culinarycraft/LoginActivity.java
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/LoginActivity.java
@@ -1,24 +1,68 @@
 package ba.sum.fpmoz.culinarycraft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText loginEmailTxt = findViewById(R.id.unesiemail);
+        EditText loginPasswordTxt= findViewById(R.id.unesiprezime);
+
+        Button loginBtn = findViewById(R.id.btnprijavise);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmailTxt.getText().toString();
+                String password = loginPasswordTxt.getText().toString();
+                if (email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getBaseContext(), "Ne smijete ostaviti prazno polje!", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(getBaseContext(), "Uspješna prijava!" , Toast.LENGTH_SHORT).show();
+                                //Intent movieIntent = new Intent(LoginActivity.this, MovieActivity.class);
+                                //startActivity(movieIntent);
+                            }else{
+                                Toast.makeText(getBaseContext(),"Pogrešno unešeni podaci!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
         TextView btn=findViewById(R.id.btn_registriraj_se);
+
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });
+
     }
 }
--- app/src/main/java/ba/sum/fpmoz/culinarycraft/RegisterActivity.java
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/RegisterActivity.java
@@ -2,23 +2,54 @@ package ba.sum.fpmoz.culinarycraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
 
 public class RegisterActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        TextView btn=findViewById(R.id.Većimateračun);
-        btn.setOnClickListener(new View.OnClickListener() {
+        EditText registerNameTxt = findViewById(R.id.unesiemail);
+        EditText registerLastNameTxt = findViewById(R.id.);
+        EditText registerEmailTxt = findViewById(R.id.unesieemail);
+        EditText registerPassword = findViewById(R.id.lo_lozinka);
+        EditText registerButton= findViewById(R.id.btnregistracija);
+        Butoon = findViewById(R.id.btnprijavise);
+
+
+
+        btnregistracija.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                final String UnesiimeTxt = Unesiime.getText().toString();
+                final String UnesiprezimeTxt = Unesiprezime.getText().toString();
+                final String UnesiemailTxt = Unesiemail.getText().toString();
+                final String UnesilozinkuTxt = Unesilozinku.getText().toString();
+
+                //probjera da li se sve popunjeno prije registracije
+                if(UnesiimeTxt.isEmpty() || UnesiprezimeTxt.isEmpty() || UnesiemailTxt.isEmpty() ||  UnesilozinkuTxt.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Molim Vas popunite sva mjesta!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        TextView btn=findViewById(R.id.Većimateračun);
+        btn_prijavi_se.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
