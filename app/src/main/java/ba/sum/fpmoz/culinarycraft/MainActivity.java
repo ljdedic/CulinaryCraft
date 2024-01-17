fdd72fc10cbf8f6ed2967c9dd2caa93056abf95f
MarijaGalic <marija.galic@fpmoz.sum.ba>
2024-01-17 08:28:33 +0100

    Dodavnje jela

--- app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
@@ -3,13 +3,29 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+    public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab=findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"USPJEŠNO",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
