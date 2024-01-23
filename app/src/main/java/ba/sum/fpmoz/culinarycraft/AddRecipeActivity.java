ec54da8044853341a4e52dc80261e2410bfc0563
Marija Galić <marija.galic@fpmoz.sum.ba>
2024-01-24 00:03:48 +0100

    Dodavanje slike

--- app/src/main/java/ba/sum/fpmoz/culinarycraft/AddRecipeActivity.java
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/AddRecipeActivity.java
@@ -1,14 +1,55 @@
 package ba.sum.fpmoz.culinarycraft;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddRecipeActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private ImageView dodajJeloImageView;
+    private Button dodajSlikuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        dodajJeloImageView = findViewById(R.id.dodajjelo);
+        dodajSlikuButton = findViewById(R.id.dodajsliku);
+
+        dodajSlikuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
+    }
+
+    private void openImagePicker() {
+        // Create an Intent to open the image gallery
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryIntent.setType("image/*");
+
+        // Start the activity for result, expecting a result in onActivityResult
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
+    }
+
+    // Handle the result of the image picker activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            // Get the selected image URI and set it to the ImageView
+            Uri imageUri = data.getData();
+            dodajJeloImageView.setImageURI(imageUri);
+        }
     }
 }
