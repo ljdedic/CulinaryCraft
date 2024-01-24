7c275b706cd755d6cd606cee18d6ffba8f7180b7
IvanJurišić <ivanjur8@gmil.com>
2024-01-24 01:03:24 +0100

    Dodavanje Heart_empty i Heart_filled

--- app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
@@ -9,12 +9,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.ImageButton;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.FirebaseDatabase;
 
     public class MainActivity extends AppCompatActivity {
+
+        ImageButton likeButton;
+        boolean isLiked = false;
+
+        public void onLikeButtonClick(View view) {
+            if (!isLiked) {
+                // Promenite ikonu srca na ispunjeno srce
+                likeButton.setImageResource(R.drawable.ic_heart_filled);
+                isLiked = true;
+                Toast.makeText(this, "Sviđa mi se!", Toast.LENGTH_SHORT).show();
+            } else {
+                // Ako je već lajkano, promenite ikonu srca na prazno srce
+                likeButton.setImageResource(R.drawable.ic_heart_empty);
+                isLiked = false;
+                Toast.makeText(this, "Uklonjeno sviđanje.", Toast.LENGTH_SHORT).show();
+            }
+        }
 
     FloatingActionButton fab;
     RecyclerView recyclearView;
