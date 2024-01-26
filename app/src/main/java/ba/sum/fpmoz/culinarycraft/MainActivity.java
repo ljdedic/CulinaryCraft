5b32a34f88492bfdd857684a6aa573901e3e12ce
IvanJurišić <ivanjur8@gmil.com>
2024-01-26 14:13:51 +0100

    Dodavanje pretraživanja u firebase

--- app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
+++ app/src/main/java/ba/sum/fpmoz/culinarycraft/MainActivity.java
@@ -3,10 +3,13 @@ package ba.sum.fpmoz.culinarycraft;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.SearchView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.ImageButton;
@@ -52,9 +55,54 @@ import com.google.firebase.database.FirebaseDatabase;
         }
 
         @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.search, menu);
+
+            // Find the SearchView using the correct ID (R.id.action_search)
+            MenuItem searchItem = menu.findItem(R.id.action_search);
+            SearchView searchView = (SearchView) searchItem.getActionView();
+
+            // Customize the SearchView if needed
+            // ...
+
+            // Set up the SearchView listener
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    txtSearch(query);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String query) {
+                    txtSearch(query);
+                    return false;
+                }
+            });
+
+            return super.onCreateOptionsMenu(menu);
+        }
+        private void txtSearch(String str)
+        {
+            FirebaseRecyclerOptions<Mainmodel> options = new FirebaseRecyclerOptions.Builder<Mainmodel>().setQuery(
+                    this.mDatabase.getReference("jela").orderByChild("imejela").startAt(str).endAt(str+"~"),Mainmodel.class)
+                    .build();
+
+            mainAdapter = new MainAdapter(options);
+            mainAdapter.startListening();
+            recyclearView.setAdapter(mainAdapter);
+        }
+
+
+        @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("CulinaryCraft");
 
         this.recyclearView = (RecyclerView)findViewById(R.id.rv);
         this.recyclearView.setLayoutManager(new LinearLayoutManager(this));
@@ -68,6 +116,12 @@ import com.google.firebase.database.FirebaseDatabase;
 
         this.mainAdapter = new MainAdapter(options);
         this.recyclearView.setAdapter(mainAdapter);
+
+
+
+
+
+
 
 
 
